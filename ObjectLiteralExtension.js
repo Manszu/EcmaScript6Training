355ae9e08ce376c1ce6589bb1b2d6370afdb9316
m4n5zu <krzysztof.mansz@gmail.com>
2017-06-04 23:40:20 +0200

    added short notation for a function

--- ObjectLiteralExtension.js
+++ ObjectLiteralExtension.js
@@ -1,5 +1,5 @@
 'use strict';
-var price = 5.99, quantity =30;
+var price = 5.99, quantity =10;
 var productView = {
   price,
   quantity
@@ -8,3 +8,13 @@ console.log(productView);
 /*output :
 {price: 5.99,
 quantity: 30} */
+
+var price = 5.99, quantity =10;
+var productView = {
+  price,
+  quantity
+  calculateValue() { //short notation for a function
+    return this.price * this.quantity
+  }
+};
+console.log(productView.calculateValue()); //return 5.99000
